719246006f62892546b5347ca95d2f435aca395c
cj291059 <cj291059@ens.uqam.ca>
2022-08-25 19:48:15 -0400

    Added unique word for users

--- venv/src/pages/views.py
+++ venv/src/pages/views.py
@@ -5,21 +5,15 @@ from django.contrib import messages
 from wordgame.models import Wordgame
 import random
 # Create your views here.
-def home_view(request):
+def home_view(request, context={}):
     print(request.user)
 
     if not request.user.is_authenticated or request.user.is_anonymous:
         return redirect('login')
-    num = random.randint(17, Wordgame.objects.count())
-    try:
-        obj = Wordgame.objects.get(id=num)
-        print(obj)
-        word = obj.word
-    except Wordgame.DoesNotExist:
-        word = "Oops! Vous aviez 1 chance sur " + str(Wordgame.objects.count()) + " de tomber sur ce mot. Vous devez appuyer sur le bouton, sinon une conséquence vous sera attribuée."
-    context = {
-        'object': word
-    }
+    
+    if request.method == 'POST':
+        set_word_variable(request)
+        return redirect('home')
     
     return render(request, "home.html", context)
 
@@ -31,7 +25,7 @@ def login_view(request):
         if user is not None:
             login(request, user)
             messages.success(request, 'You are now logged in')
-
+            request = set_word_variable(request)
             return redirect('home')
         else:
             messages.error(request, 'Mauvais identifiants!')
@@ -40,3 +34,29 @@ def login_view(request):
         return redirect('home')
     
     return render(request, 'login.html', {})
+
+def set_word_variable(request):
+    num = random.randint(17, Wordgame.objects.count())
+    try:
+        obj = Wordgame.objects.get(id=num)
+        print(obj)
+        word = obj.word
+    except Wordgame.DoesNotExist:
+        word = "Oops! Vous aviez 1 chance sur " + str(Wordgame.objects.count()) + " de tomber sur ce mot. Vous devez appuyer sur le bouton, sinon une conséquence vous sera attribuée."
+
+    request.session['word'] = word
+    return request
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+def words_detail_view(request):
+    if not request.user.is_authenticated or request.user.is_anonymous:
+        return redirect('login')
+    
+    if request.method == 'POST':
+        set_word_variable(request)
+        return redirect('wordgame')
+    
+    return render(request, 'wordgame.html', {})
